9401464866f25923834462c6b1af54bed43ff9dc
khidirbekov <tacata97@live.com>
2024-07-22 20:47:17 +0300

    update lib

--- dist/crypto-pro-aft.d.ts
+++ dist/crypto-pro-aft.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for crypto-pro-aft 2.3.5
+// Type definitions for crypto-pro-aft 2.3.6
 // Project: crypto-pro-aft
 // Definitions by: Khadzhimurad Khidirbekov https://github.com/khidirbekov
 
